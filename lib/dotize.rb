1c5f3d0c331aa158f870fdec8126230b89e45728
odlp <oliverp@gmail.com>
2016-01-09 17:22:31 +0000

    Initial implementation

--- lib/dotize.rb
+++ lib/dotize.rb
@@ -1,5 +1,28 @@
 require "dotize/version"
 
 module Dotize
-  # Your code goes here...
+  def dot(selector, &default_block)
+    current_value = self
+
+    begin
+      keys_from_selector(selector).each do |key|
+        raise StopIteration unless current_value.respond_to? :fetch
+        current_value = current_value.fetch(key) { raise StopIteration }
+      end
+    rescue StopIteration
+      return calc_default_value(default_block)
+    end
+
+    current_value
+  end
+
+  private
+
+  def keys_from_selector(selector)
+    selector.split('.')
+  end
+
+  def calc_default_value(default_block)
+    default_block.call(self) unless default_block.nil?
+  end
 end
--- spec/dotize_spec.rb
+++ spec/dotize_spec.rb
@@ -5,7 +5,93 @@ describe Dotize do
     expect(Dotize::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '#dot' do
+    before(:each) { subject.extend(Dotize) }
+
+    describe 'finding a property' do
+      subject do
+        { 'a' => 123 }
+      end
+
+      it 'returns the corresponding value' do
+        expect(subject.dot('a')).to equal(123)
+      end
+    end
+
+    describe 'handling falsey values' do
+      subject do
+        { 'a' => false, 'b' => nil }
+      end
+
+      it 'returns false' do
+        result = subject.dot('a') { raise 'Default should not be called' }
+        expect(result).to eq(false)
+      end
+
+      it 'returns nil' do
+        result = subject.dot('b') { raise 'Default should not be called' }
+        expect(result).to be_nil
+      end
+    end
+
+    context 'nested property' do
+      subject do
+        { 'a' => { 'b' => 456 } }
+      end
+
+      it 'returns the corresponding value' do
+        expect(subject.dot('a.b')).to equal(456)
+      end
+
+      describe 'when the property does not exist' do
+        it 'returns nil' do
+          expect(subject.dot('a.z')).to be_nil
+        end
+      end
+
+      describe 'when the selector matches a non-hash-like object midway' do
+        it 'returns nil' do
+          expect(subject.dot('a.b.z')).to be_nil
+        end
+      end
+    end
+
+    describe 'default block' do
+      subject do
+        { 'a' => 123 }
+      end
+
+      describe 'when a property is not found' do
+        it 'returns the value from the block provided' do
+          result = subject.dot('z') { 2 + 2 }
+          expect(result).to equal(4)
+        end
+
+        it 'passes the subject to the block' do
+          result = subject.dot('z') { |hash| hash.keys }
+          expect(result).to contain_exactly('a')
+        end
+
+        it 'does not calculate the default multiple times' do
+          times_block_called = 0
+          default_block = Proc.new do
+            times_block_called += 1
+            2 + 2
+          end
+
+          result = subject.dot('z.z.z.z', &default_block)
+
+          expect(result).to eq(4)
+          expect(times_block_called).to eq(1)
+        end
+      end
+
+      describe 'when all properties found' do
+        it 'does not call the default block' do
+          result = subject.dot('a') { raise 'Default should not be called' }
+          expect(result).to equal(123)
+        end
+      end
+    end
   end
 end
